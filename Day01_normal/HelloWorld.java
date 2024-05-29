8e89f4863bce2809dbdaaf59bf711fb644269579
Shen Liwei <1925701506@qq.com>
2024-05-29 18:38:21 +0300

    ADD HelloWorld

---
+++ Day01_normal/HelloWorld.java
@@ -0,0 +1,38 @@
+package Day01_normal;
+
+public class HelloWorld {
+    //main+回车
+    public static void main(String[] args) {
+   //sout+回车
+        System.out.println();
+        System.out.println();//Ctrl+D 复制数据到下一行
+        //Ctrl+Alt+L 格式化代码，建议自己写注释掉就行
+        //Ctrl+/ 对选中的代码添加单行注释
+        //Ctrl+Shift+/ 对对选中代码进行多行注释
+
+        //整数
+        System.out.println(666);
+        System.out.println(-18);
+
+        //字符串
+        System.out.println("Hello World");
+        System.out.println("我是最厉害的学习者");
+
+        //字符
+        System.out.println('A');
+        System.out.println('0');
+        System.out.println('我');
+
+        //布尔
+        System.out.println(true);
+        System.out.println(false);
+
+        //变量
+        int price = 888;
+        double unit = 666.66;
+        System.out.println(price);
+        System.out.println(unit);
+        unit = 999.99;
+        System.out.println(unit);
+    }
+}
